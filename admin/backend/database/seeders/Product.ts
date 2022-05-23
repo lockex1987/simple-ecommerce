a292ced60be51ab3e7ee748c06b8c736ab5ad2f0
Nguyen Van Huyen <lockex1987@gmail.com>
2022-05-23 09:10:08 +0700

    Seed product

--- admin/backend/database/seeders/Product.ts
+++ admin/backend/database/seeders/Product.ts
@@ -1,9 +1,25 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import Product from 'App/Models/Product'
+import axios from 'axios'
 
 export default class ProductSeeder extends BaseSeeder {
   public async run() {
-    const productList = []
+    const options = {
+      proxy: {
+        protocol: 'http',
+        host: '192.168.103.26',
+        port: 80,
+      },
+    }
+    const { data } = await axios.get('https://fakestoreapi.com/products', options)
+    console.log(data)
+
+    const productList = data.map(e => ({
+      title: e.title,
+      description: e.description,
+      price: e.price,
+      image: e.image,
+    }))
     await Product.createMany(productList)
   }
 }
